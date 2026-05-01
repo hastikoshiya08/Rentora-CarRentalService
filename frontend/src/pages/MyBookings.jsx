159a0c8652d50d4c864dee9350a34e621e0a2b69
Hasti18 <hastikoshiya08@gmail.com>
2026-05-01 12:35:20 +0530

    Fix filter button background UI

--- frontend/src/pages/MyBookings.jsx
+++ frontend/src/pages/MyBookings.jsx
@@ -118,7 +118,7 @@ const handleInvoice = (b) => {
       </h1>
 
       {/* FILTER */}
-      <div className="flex justify-center gap-3 flex-wrap">
+      <div className="flex justify-center bg-black gap-3 flex-wrap">
         {["All", "Upcoming", "Completed", "Cancelled"].map((filter) => (
           <button
             key={filter}
