f68a1613ce1a2021c593b890a4d989462ea5d67a
Hasti18 <hastikoshiya08@gmail.com>
2026-05-01 12:48:53 +0530

    Fix filter button background UI

--- frontend/src/pages/MyBookings.jsx
+++ frontend/src/pages/MyBookings.jsx
@@ -126,7 +126,7 @@ const handleInvoice = (b) => {
             className={`px-5 py-2 rounded-full ${
               selectedFilter === filter
                 ? "bg-yellow-500 text-black"
-                : "bg-black-700 text-white"
+                : "bg-gray-700 text-white"
             }`}
           >
             {filter}
